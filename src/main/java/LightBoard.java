5f280a9bf7e350769c5e89f2d849870d0319ba52
Yiwang5 <yiwang5@s.sfusd.edu>
2025-04-30 09:55:29 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,7 +10,11 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
-    /* to be implemented in part (a) */
+    lights = new boolean[numRows][numCols];
+    for(int r = 0; r < numRows; r++)
+      for(int c = 0; c < numCols; c++)
+        double rnd =Math.Random();
+        lights[r][c] = rnd < 0.4;
 
   }
 
