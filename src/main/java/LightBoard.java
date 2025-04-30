1b8ce9c411a8edbfa43c0fcf1f747e21335e04de
Yiwang5 <yiwang5@s.sfusd.edu>
2025-04-30 14:44:17 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -39,7 +39,7 @@ public class LightBoard
     if(lights[row][col] && numOn % 2 == 0){
    return false;
     }
-    if(lights[rol][col] && numOn % 3 == 0){
+    if(lights[row][col] && numOn % 3 == 0){
       return true;
     }
     return lighbts[row][col];
